b06053539620b556383f63a3cd6dad8c25896fc7
Johannes <johannes.idarsson@gmail.com>
2023-11-05 15:42:02 +0100

    implemented server endpoints

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 import "dotenv/config";
 import Server from "./server.js";
+import * as fs from "fs";
 
 function setup() {
     const server = new Server(process.env.HOST, process.env.PORT);
 
+    const ledStatusMap = new Map();
+
     server.addEndpoint("/dashboard", "GET", () => {
-        // TODO: Serve dashboard
-        console.log("Get request received");
+        return {
+            status: 200,
+            headers: {
+                "Content-Type": "text/html"
+            },
+            data: fs.readFileSync("dashboard.html")
+        };
     });
 
     server.addEndpoint("/", "GET", () => {
@@ -19,13 +27,38 @@ function setup() {
     });
 
     server.addEndpoint("/status/:ledId", "POST", (requestData) => {
-        // TODO: Set status
-        console.log("Got a post with the following request data", requestData);
+        if (!requestData.dynamicParameters) {
+            return { status: 500 };
+        }
+
+        const { ledId } = requestData.dynamicParameters;
+        const isEnabled = requestData.parsedBody === "1";
+
+        if (isEnabled) {
+            ledStatusMap.set(ledId, "1");
+        } else {
+            ledStatusMap.set(ledId, "0");
+        }
     });
 
-    server.addEndpoint("/status/:ledId", "GET", () => {
-        // TODO: Get status
-        console.log("Got a GET asking for the LED status");
+    server.addEndpoint("/status/:ledId", "GET", (requestData) => {
+        if (!requestData.dynamicParameters) {
+            return { status: 500 };
+        }
+
+        const { ledId } = requestData.dynamicParameters;
+
+        // Should perhaps check if the led exists in the first place, and throw an error if not
+        // On the other hand, if there is no led, it will always be turned off
+        const ledStatus = ledStatusMap.get(ledId) || "0";
+
+        return {
+            status: 200,
+            headers: {
+                "Content-Type": "text/plain"
+            },
+            data: ledStatus === "1" ? "1" : "0"
+        };
     });
 }
 
